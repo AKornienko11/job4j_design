b27fe6dceb5166aec8d2f53bc917c8db19edcf62
AKornienko3 <Kornienko.srs@mail.ru>
2024-04-03 15:07:42 +0500

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -0,0 +1,114 @@
+package ru.job4j.collection;
+
+import java.beans.Introspector;
+import java.util.*;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+    private T[] container;
+    private int size;
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        container = (T[]) new Object[capacity];
+    }
+
+
+    @Override
+    public void add(T value) {
+
+        if (container.length <= size) {
+            container = Arrays.copyOf(container, container.length * 2);
+        }
+        if (container.length == 0) {
+            container = (T[]) new Object[1];
+        }
+        container[size++] = value;
+        modCount++;
+    }
+
+
+    @Override
+    public void set(int index, T newValue) {
+        if (container.length == size) {
+            container = Arrays.copyOf(container, container.length * 2);
+        }
+//
+        System.arraycopy(container, index, container, index + 1, size - index);
+        container[index] = newValue;
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public T remove(int index) {
+        if (container[index] == null) {
+            throw new IndexOutOfBoundsException();
+        }
+        T result = container[index];
+        System.arraycopy(container, index + 1, container, index, container.length - index - 1);
+        container[container.length - 1] = null;
+        size--;
+        modCount++;
+        return result;
+
+    }
+
+    @Override
+    public T get(int index) {
+        T result = null;
+        if (container[index] == null) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (Objects.checkIndex(index, container.length) < container.length) {
+            result = container[index];
+        }
+
+        return result;
+    }
+
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+
+        return new Iterator<T>() {
+            final int expectedModCount = modCount;
+
+            int count = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (modCount != expectedModCount) {
+                    throw new ConcurrentModificationException();
+                }
+                boolean res = false;
+                for (T v : container) {
+                    if (v != null) {
+                        res = true;
+                        break;
+                    }
+
+                }
+                return count < size && res;
+
+            }
+
+            @Override
+            public T next() {
+                if (container[count] == null) {
+                    throw new NoSuchElementException();
+                }
+                if (!hasNext()) {
+                    throw new IndexOutOfBoundsException();
+                }
+                return container[count++];
+            }
+        };
+    }
+
+}
+
---
+++ src/main/java/ru/job4j/list/ListUsage.java
@@ -0,0 +1,34 @@
+package ru.job4j.list;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class ListUsage {
+    public static void main(String[] args) {
+        List<String> result = Arrays.asList("one", "two", "three");
+
+        result.set(1, "two and second");
+
+        Iterator<String> iterator = result.iterator();
+        while (iterator.hasNext()) {
+            System.out.println("Текущий элемент " + iterator.next());
+        }
+
+        Integer[] data = {1, 2, 3, 3, 4, 5, 6, 7, 8};
+        Integer[] desta = new Integer[10];
+        System.arraycopy(data, 2, desta, 0, 3);
+        for (Integer res  : desta) {
+            System.out.println(res);
+        }
+        Integer[] res = Arrays.copyOf(desta, 5);
+        for (Integer v : res) {
+            System.out.print(" " + v + " ");
+        }
+
+
+
+    }
+}
+
