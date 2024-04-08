22cfeedd9d018830b1fd7931c20e12e7b225a1c2
AKornienko3 <Kornienko.srs@mail.ru>
2024-04-09 00:26:14 +0500

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,80 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleLinkedList<E> implements SimpleLinked<E> {
+
+    private int size;
+    private int modCount;
+    private Node<E> head;
+
+    public SimpleLinkedList() {
+        head = new Node<>(null, null);
+    }
+
+    @Override
+    public void add(E value) {
+        Node<E> newNode = new Node<>(value, null);
+        Node<E> h = head;
+        if (h == null) {
+            head = newNode;
+        } else {
+            while (h.next != null) {
+                h = h.next;
+            }
+            h.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> node = head;
+        for (int i = 0; i <= index; i++) {
+            node = node.next;
+        }
+        return node.item;
+    }
+
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            int expectedModCount = modCount;
+            int count = 0;
+            Node<E> h = head;
+
+            @Override
+            public boolean hasNext() {
+                if (modCount != expectedModCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return count < size;
+            }
+
+            @Override
+            public E next() {
+                if (hasNext()) {
+
+                    h = h.next;
+                    E item = h.item;
+                    count++;
+                    return item;
+                }
+                return null;
+            }
+        };
+    }
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+
+        Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
