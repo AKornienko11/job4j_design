0a096d2f47cd43b38e8a6e91ccdd7ee5feaee893
AKornienko3 <Kornienko.srs@mail.ru>
2024-09-01 21:46:33 +0500

    8. Тестирование IO [#173905]

--- src/test/java/ru/job4j/io/AnalysisTest.java
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -16,12 +16,12 @@ class AnalysisTest {
         Analysis analysis = new Analysis();
         File source = tempDir.resolve("source.txt").toFile();
         try (PrintWriter output = new PrintWriter(source)) {
-            output.println("200 10:56:01\n" +
-                    "500 10:57:01\n" +
-                    "400 10:58:01\n" +
-                    "500 10:59:01\n" +
-                    "400 11:01:02\n" +
-                    "300 11:02:02");
+            output.println("200 10:56:01"
+                    + "500 10:57:01"
+                    + "400 10:58:01"
+                    + "500 10:59:01"
+                    + "400 11:01:02"
+                    + "300 11:02:02");
 
         }
         File target = tempDir.resolve("target.txt").toFile();
@@ -40,11 +40,11 @@ class AnalysisTest {
         Analysis analysis = new Analysis();
         File source = tempDir.resolve("source.txt").toFile();
         try (PrintWriter output = new PrintWriter(source)) {
-            output.println("200 10:56:01\n" +
-                    "500 10:57:01\n" +
-                    "400 10:58:01\n" +
-                    "300 10:59:01\n" +
-                    "500 11:01:02\n" +
+            output.println("200 10:56:01" +
+                    "500 10:57:01" +
+                    "400 10:58:01" +
+                    "300 10:59:01" +
+                    "500 11:01:02" +
                     "200 11:02:02");
 
         }
@@ -58,4 +58,5 @@ class AnalysisTest {
         }
         assertThat("10:57:01 10:59:01;11:01:02 11:02:02;").hasToString(result.toString());
     }
+
 }
