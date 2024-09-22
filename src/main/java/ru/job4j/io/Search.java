22388696887e007e9b74293f5e912c33fef0433e
AKornienko3 <Kornienko.srs@mail.ru>
2024-09-22 22:10:53 +0500

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,24 +1,36 @@
 package ru.job4j.io;
 
-import java.awt.desktop.PrintFilesEvent;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Path.of("C:\\projects\\job4j_design\\src\\main\\java\\ru\\job4j\\io\\files");
+        validate(args);
+        Path start = Path.of(args[0]);
         search(start, path -> path.toFile().getName().endsWith(".txt")).forEach(System.out::println);
     }
-
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
     }
+
+    public static void validate(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        if (!args[1].equals(".txt")) {
+            throw new IllegalArgumentException("There is no such type in the arguments");
+        }
+    }
 }
+
+
+
+
+
+
