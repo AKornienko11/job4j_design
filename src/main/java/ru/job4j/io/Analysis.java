274f7398395f7b5111b05b7a2143f374b11bfa85
AKornienko11 <Kornienko.srs@mail.ru>
2025-06-14 13:27:34 +0500

    12. Валидация параметров запуска Class Dir.

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -6,24 +6,23 @@ import java.util.StringJoiner;
 public class Analysis {
     public void unavailable(String source, String target) {
         StringJoiner stringJoiner = new StringJoiner(System.lineSeparator());
-        try (PrintWriter stream = new PrintWriter(new FileOutputStream(target));
+        try (PrintWriter printWriter = new PrintWriter(new FileOutputStream(target));
              BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
             bufferedReader.lines().forEach(stringJoiner::add);
             String[] str = stringJoiner.toString().split(System.lineSeparator());
-            for (int i = 0; i < str.length; i++) {
-                if (str[i].contains("400") || str[i].contains("500")) {
-                    String[] time = str[i].split(" ");
-                    stream.write(time[1] + " ");
-                    i++;
-                    for (int j = 0; j < str.length; j++) {
-                        if (str[i].contains("400") || str[i].contains("500")) {
-                            i++;
-                        } else {
-                            String[] end = str[i].split(" ");
-                            stream.write(end[1] + ";" + System.lineSeparator());
-                            break;
-                        }
-                    }
+            boolean flag = true;
+            String res;
+            for (String s : str) {
+                if ((s.contains("400") || s.contains("500")) && flag) {
+                    res = s.split(" ")[1];
+                    printWriter.printf("%s%s", res, ";");
+                    flag = false;
+                }
+                if ((s.contains("200") || s.contains("300")) && !flag) {
+                    res = s.split(" ")[1];
+                    printWriter.printf("%s%s", res, ";");
+                    flag = true;
+                    printWriter.print(System.lineSeparator());
                 }
 
             }
