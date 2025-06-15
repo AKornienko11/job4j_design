3bc210123bb8c31a9575ed1cdfaae2a66312bcf5
AKornienko11 <Kornienko.srs@mail.ru>
2025-06-15 19:54:00 +0500

    2. Что такое Socket? [#4850 #546102]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -21,7 +21,6 @@ public class EchoServer {
                         System.out.println(string);
                     }
                     output.flush();
-
                 }
             }
         }
